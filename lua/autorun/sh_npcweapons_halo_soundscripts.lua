f288721f3519e5e306f2058279d8b274423c5713
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-25 03:04:40 -0700

    Add soundscripts

--- lua/autorun/sh_npcweapons_halo_soundscripts.lua
+++ lua/autorun/sh_npcweapons_halo_soundscripts.lua
@@ -9,3 +9,85 @@ sound.Add({
 	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
 	sound = "weapons/npcweapons_halo_ar/ar-1.wav"
 })
+--HALO BATTLE RIFLE--
+sound.Add({
+	name = "swep_ai_halo_br_fire_1",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_br/br-1.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_br_fire_2",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_br/br-2.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_br_fire_3",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_br/br-3.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_br_fire_4",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_br/br-4.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_br_fire_5",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_br/br-5.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_br_fire_6",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_br/br-6.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_br_fire_7",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_br/br-7.wav"
+})
+--HALO CARBINE--
+sound.Add({
+	name = "swep_ai_halo_carbine_fire_1",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_carbine/carbine-1.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_carbine_fire_2",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_carbine/carbine-2.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_carbine_fire_3",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_carbine/carbine-3.wav"
+})
