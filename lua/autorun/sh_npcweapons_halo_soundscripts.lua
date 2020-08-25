1692f621233ec8de330f048a18d049ec5d614ec6
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-25 03:12:56 -0700

    Update Assault Rifle

--- lua/autorun/sh_npcweapons_halo_soundscripts.lua
+++ lua/autorun/sh_npcweapons_halo_soundscripts.lua
@@ -2,12 +2,28 @@ include("autorun/sh_npcweapons_constants.lua")
 
 --HALO ASSAULT RIFLE--
 sound.Add({
-	name = "swep_ai_halo_ar_fire",
+	name = "swep_ai_halo_ar_fire_1",
 	channel = CHAN_WEAPON,
 	volume = NPC_WEAPONS_VOLUME_MAX,
 	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
 	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
 	sound = "weapons/npcweapons_halo_ar/ar-1.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_ar_fire_2",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_ar/ar-2.wav"
+})
+sound.Add({
+	name = "swep_ai_halo_ar_fire_3",
+	channel = CHAN_WEAPON,
+	volume = NPC_WEAPONS_VOLUME_MAX,
+	level = NPC_WEAPONS_SNDLVL_GUNFIRE,
+	pitch = NPC_WEAPONS_PITCH_GUNFIRE,
+	sound = "weapons/npcweapons_halo_ar/ar-3.wav"
 })
 --HALO BATTLE RIFLE--
 sound.Add({
