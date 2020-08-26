bce9e83e19cee8774fa9a8a3bf9978128ca48c64
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-26 06:12:07 -0700

    Remove error sound

--- lua/autorun/sh_npcweapons_halo_setup.lua
+++ lua/autorun/sh_npcweapons_halo_setup.lua
@@ -5,7 +5,6 @@ if not file.Exists("weapons/swep_ai_base/shared.lua", "LUA") then
 		timer.Simple(0, function()
 
 			notification.AddLegacy("NPC Weapons Base is not installed.", NOTIFY_ERROR, 10)
-			surface.PlaySound("buttons/button10.wav")
 
 			chat.AddText(
 				Color(255, 32, 32),
