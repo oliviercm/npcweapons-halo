70662e62c754bc3ddd95e8be330ce9479b5b243f
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-26 04:40:03 -0700

    Remove beam rifle

--- lua/autorun/sh_npcweapons_halo_weaponlist.lua
+++ lua/autorun/sh_npcweapons_halo_weaponlist.lua
@@ -1,5 +1,4 @@
 list.Add("NPCUsableWeapons", {class = "swep_ai_halo_ar", title = "[NPCW] HALO ASSAULT RIFLE"})
-list.Add("NPCUsableWeapons", {class = "swep_ai_halo_beam_rifle", title = "[NPCW] HALO BEAM RIFLE"})
 list.Add("NPCUsableWeapons", {class = "swep_ai_halo_br", title = "[NPCW] HALO BATTLE RIFLE"})
 list.Add("NPCUsableWeapons", {class = "swep_ai_halo_bruteshot", title = "[NPCW] HALO BRUTESHOT"})
 list.Add("NPCUsableWeapons", {class = "swep_ai_halo_carbine", title = "[NPCW] HALO CARBINE"})
