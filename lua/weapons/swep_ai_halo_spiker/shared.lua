3b053413bc27340ada922df62163fad7ddb8f679
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-26 02:34:30 -0700

    Add more weapons and models

---
+++ lua/weapons/swep_ai_halo_spiker/shared.lua
@@ -0,0 +1,43 @@
+DEFINE_BASECLASS("swep_ai_base")
+
+--[[
+--HALO SPIKER
+--]]
+
+SWEP.WorldModel					= "models/weapons/w_rif_famas.mdl"
+SWEP.HoldType					= "smg"
+
+SWEP.MuzzleEffect    			= "MuzzleEffect"
+SWEP.EnableShellEffect			= false
+SWEP.TracerEffect				= "Tracer"
+SWEP.ReloadSounds				= {}
+
+SWEP.ReloadTime					= NPC_WEAPONS_RELOAD_TIME_MED
+SWEP.Primary.DamageMin			= 3
+SWEP.Primary.DamageMax			= 5
+SWEP.Primary.MinDropoffDistance	= NPC_WEAPONS_MIN_DROPOFF_DISTANCE_SMG
+SWEP.Primary.MaxDropoffDistance	= NPC_WEAPONS_MAX_DROPOFF_DISTANCE_SMG
+SWEP.Primary.Force				= 5
+SWEP.Primary.Spread				= 0.060
+SWEP.Primary.SpreadMoveMult		= NPC_WEAPONS_SPREAD_MOVE_MULT_NONE
+SWEP.Primary.BurstMinShots		= 3
+SWEP.Primary.BurstMaxShots		= 5
+SWEP.Primary.BurstMinDelay		= 0
+SWEP.Primary.BurstMaxDelay		= 0
+SWEP.Primary.FireDelay			= 0.07
+SWEP.Primary.NumBullets			= 1
+SWEP.Primary.ClipSize			= 60
+SWEP.Primary.DefaultClip		= 60
+SWEP.Primary.AimDelayMin		= NPC_WEAPONS_MIN_AIM_DELAY_LOW
+SWEP.Primary.AimDelayMax		= NPC_WEAPONS_MAX_AIM_DELAY_LOW
+SWEP.Primary.Sound				= "swep_ai_halo_smg_fire_1"
+
+SWEP.ClientModel				= {
+	model						= "models/spiker.mdl",
+	pos							= Vector(4, 1.399, -1.364),
+	angle						= Angle(180, 270, 0),
+	size						= Vector(1.2, 1.2, 1.2),
+	color						= Color(255, 255, 255, 255),
+	skin						= 0,
+	bodygroup					= {},
+}
