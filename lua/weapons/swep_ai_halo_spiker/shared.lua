7c58f8fabe7e7f62520f2bc8d547841099c86bc7
Olivier Chan <olivierchanmoy@gmail.com>
2020-08-29 00:38:01 -0700

    Update Spiker to use new projectile functionality

--- lua/weapons/swep_ai_halo_spiker/shared.lua
+++ lua/weapons/swep_ai_halo_spiker/shared.lua
@@ -13,6 +13,9 @@ SWEP.TracerEffect				= "Tracer"
 SWEP.ReloadSounds				= {{0.4, "swep_ai_halo_spiker_reload"}}
 
 SWEP.ReloadTime					= NPC_WEAPONS_RELOAD_TIME_MED
+
+SWEP.Primary.Type               = "projectile"
+SWEP.Primary.DamageType         = DMG_GENERIC
 SWEP.Primary.DamageMin			= 8
 SWEP.Primary.DamageMax			= 10
 SWEP.Primary.MinDropoffDistance	= NPC_WEAPONS_MIN_DROPOFF_DISTANCE_SMG
@@ -32,15 +35,19 @@ SWEP.Primary.AimDelayMin		= NPC_WEAPONS_MIN_AIM_DELAY_LOW
 SWEP.Primary.AimDelayMax		= NPC_WEAPONS_MAX_AIM_DELAY_LOW
 SWEP.Primary.Sound				= {"swep_ai_halo_spiker_fire_1", "swep_ai_halo_spiker_fire_2", "swep_ai_halo_spiker_fire_3", "swep_ai_halo_spiker_fire_4", "swep_ai_halo_spiker_fire_5", "swep_ai_halo_spiker_fire_6"}
 
-SWEP.ProjectileModel				= "models/crossbow_bolt.mdl"
-SWEP.ProjectileSpeed				= 2000
-SWEP.ProjectileAcceleration			= 0
-SWEP.ProjectileDamageType			= DMG_GENERIC
-SWEP.ProjectileHitEffect			= "MetalSpark"
-SWEP.ProjectileHitEffectScale		= 0.5
-SWEP.ProjectileHitEffectMagnitude	= 0.5
-SWEP.ProjectileHitEffectRadius		= 0.5
-SWEP.ProjectileHitSound				= "swep_ai_halo_spike_impact"
+SWEP.AimForBody     			= true
+
+SWEP.ProjectileModel            = "models/crossbow_bolt.mdl"
+SWEP.ProjectileModelScale       = 0.75
+SWEP.ProjectileStartSpeed       = 2000
+SWEP.ProjectileAcceleration	    = 0
+SWEP.ProjectileHitEffect        = { Name = "MetalSpark", Radius = 0.5, Magnitude = 0.5, Scale = 0.5 }
+SWEP.ProjectileHitWorldEffect   = { Name = "BoltImpact", Radius = 1, Magnitude = 1, Scale = 1, ReverseForward = true, TraceThrough = 128 }
+SWEP.ProjectileHitSound         = { Sound = "swep_ai_halo_spike_impact" }
+SWEP.ProjectileLoopingSound     = false
+SWEP.ProjectileRotationSpeed    = 0
+SWEP.ProjectileIsExplosive      = false
+SWEP.ProjectileTrail            = false
 
 SWEP.ClientModel				= {
 	model						= "models/spiker.mdl",
@@ -51,41 +58,3 @@ SWEP.ClientModel				= {
 	skin						= 0,
 	bodygroup					= {},
 }
-
-function SWEP:Shoot()
-
-	local owner = self:GetOwner()
-	local enemy = owner:GetEnemy()
-	local muzzlePos = owner:GetPos():Distance(enemy:GetPos()) > 128 and self:GetAttachment(self.MuzzleAttachment).Pos or owner:WorldSpaceCenter()
-	local targetPos = enemy:BodyTarget(muzzlePos) or enemy:WorldSpaceCenter()
-	local inaccuracy = self.Primary.Spread
-	local shootAngle = Vector(targetPos.x - muzzlePos.x, targetPos.y - muzzlePos.y, targetPos.z - muzzlePos.z):Angle()
-	shootAngle.p = shootAngle.p + math.Rand(-inaccuracy, inaccuracy)
-	shootAngle.y = shootAngle.y + math.Rand(-inaccuracy, inaccuracy)
-	
-	local projectile = ents.Create("ai_crossbow_projectile")
-	projectile:SetPos(muzzlePos)
-	projectile:SetAngles(shootAngle)
-	projectile:SetOwner(owner)
-	projectile.Model = self.ProjectileModel
-	projectile.Damage = math.random(self.Primary.DamageMin, self.Primary.DamageMax)
-	projectile.DamageType = self.ProjectileDamageType
-	projectile.Force = self.Primary.Force
-	projectile.Speed = self.ProjectileSpeed
-	projectile.Acceleration = self.ProjectileAcceleration
-	projectile.Owner = owner
-	projectile.ProjectileHitEffect = self.ProjectileHitEffect
-	projectile.ProjectileHitEffectScale = self.ProjectileHitEffectScale
-	projectile.ProjectileHitEffectMagnitude = self.ProjectileHitEffectMagnitude
-	projectile.ProjectileHitEffectRadius = self.ProjectileHitEffectRadius
-	projectile.ProjectileHitSound = self.ProjectileHitSound
-	
-	projectile:Spawn()
-
-	projectile:SetModelScale(0.75)
-	
-	self:ShootEffects()
-	
-	self:TakePrimaryAmmo(1)
-
-end
